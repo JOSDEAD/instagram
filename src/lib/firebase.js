24e7ede085bb37a89b582b28f2ec32042605061e
JOSDEAD <jmmurillo61@gmail.com>
2021-08-01 20:08:55 -0600

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,15 @@ import 'firebase/firestore';
 import 'firebase/auth' 
 
 const config={
+    apiKey: "AIzaSyBdmiKU6s3w8xGsDcyTBMdKwVvU8BYBY84",
+    authDomain: "instragram-817d8.firebaseapp.com",
+    projectId: "instragram-817d8",
+    storageBucket: "instragram-817d8.appspot.com",
+    messagingSenderId: "798786358263",
+    appId: "1:798786358263:web:2c70ad603fe43373e43cf2"
 };
 const firebase = Firebase.initializeApp(config);
 const {FieldValue} = Firebase.firestore;
+
+
 export {firebase,FieldValue}
